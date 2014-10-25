930b40ccdb8d049bbc92d8f2013f7e668538166a
gagata <agata.cieplik@gmail.com>
2014-10-25 22:08:39 +0200

    open file

--- facebook_files/static/display_files.js
+++ facebook_files/static/display_files.js
@@ -3,12 +3,18 @@ function present_files(files) {
         $(this).empty();
 
         for (var i = 0; i < files.length; i++) {
-            var div = $("<div/>").addClass("file").append("<div class=\"icon\"></div>");
-            var name = $("<div/>").text("File name").attr("id", files[i].link).addClass("name");
-            div.append(name);
-            var date = $("<div/>").text(files[i].date).addClass("date");
-            div.append(date);
-            $(this).append(div);
+            $.ajax({
+                url: files[0].link,
+                success: function(data) {
+                    var matches = data.match(/<title>(.*?)<\/title>/);
+                    var div = $("<div/>").addClass("file").append("<div class=\"icon\"></div>");
+                    var name = $("<div/>").text(matches[0]).attr("id", files[i].link).addClass("name");
+                    div.append(name);
+                    var date = $("<div/>").text(files[i].date).addClass("date");
+                    div.append(date);
+                    $(this).append(div);
+                }   
+            });
         }
         $(this).fadeIn();
 
