75de0ae58cd816f5267a99d2f2727ae4ddff230f
michald <mduczynski@student.uw.edu.pl>
2014-10-25 21:45:39 +0200

    nknjjj m

--- facebook_files/static/groups.js
+++ facebook_files/static/groups.js
@@ -63,7 +63,7 @@ function testAPI() {
 }
 
 function checkIfFile(fileName){
-    var file_hosting_prefixes = ['https://docs.google'];
+	var file_hosting_prefixes = ['https://docs.google', 'https://drive.google'];
     for(var i=0; i<file_hosting_prefixes.length; i++){
         if(fileName.indexOf(file_hosting_prefixes[i]) === 0){
             return true;
@@ -72,33 +72,45 @@ function checkIfFile(fileName){
     return false;
 }
 
+function addToFiles(files, item){
+	var msg = item.message;
+		var link_reg = /http\S*/;
+		var myArray = link_reg.exec(msg);
+		if(myArray){
+			for(var j=0; j<myArray.length; j++){
+				if(checkIfFile(myArray[j])){
+					files.push({
+						'link': myArray[j], 
+						'post': msg,
+						'date': item.created_time,
+						'from': item.from
+						})
+				}
+			}
+		}
+}
+
 function displayFolder(groupId){
     console.log("wchodzi tutaj");
     FB.api('/'+groupId+'/feed', function(response) {
-        console.log(response);
-        var data = response['data'];
-
-        console.log(data);
-        var files = [];
-        for(var i=0; i<data.length; i++){
-            var msg = data[i].message;
-            var link_reg = /http\S*/;
-            var myArray = link_reg.exec(msg);
-            if(myArray){
-                for(var j=0; j<myArray.length; j++){
-                    if(checkIfFile(myArray[j])){
-                        files.push({
-                            'link': myArray[j], 
-                            'post': msg,
-                            'date': data[i].created_time,
-                            'from': data[i].from
-                        })
-                    }
-                }
-            }
-
-        }
-        console.log(files);
+		console.log(response);
+	  	var data = response['data'];
+	  	var files = [];
+	  	var posts = [];
+	  	for(var i=0; i<data.length; i++){
+			if(data[i].comments){
+				var comments = data[i].comments.data;
+				for(var i=0; i<comments.length; i++){
+					posts.push(comments[i]);
+				}
+			}
+			posts.push(data[i]);
+	  	}
+	  	console.log(posts);
+	  	for(var i=0; i<posts.length; i++){
+			addToFiles(files, posts[i]);
+	  	}
+	  	console.log(files);
         present_files(files);
         return files;
     });
