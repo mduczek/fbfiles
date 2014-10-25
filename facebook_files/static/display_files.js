4b3d3182563fb2dd4265f8c5837b0875efb9e613
gagata <agata.cieplik@gmail.com>
2014-10-25 23:10:26 +0200

    preview

--- facebook_files/static/display_files.js
+++ facebook_files/static/display_files.js
@@ -1,15 +1,47 @@
+var EMBEDLY_KEY = "0b64e3ab10db4b17ae716b327dafeb55";   
+
+function isGoogleDriveFile(url) {
+    if (url.search("google.com") !== -1)
+        return true;
+    return false;
+}
+
+function prepareGooglePreview(file) {
+    var div = $("<div/>").addClass("file").attr("id", file.link);
+    var iframe = $("<iframe/>").attr("src", file.link).addClass("iframe");
+    div.append(iframe);
+    var date = $("<div/>").text(file.date).addClass("date");
+    div.append(date);
+    var link = $("<div/>").text("Show");
+    div.append(link);
+    return div;
+}
+
+function prepareOtherFilePreview(file) {
+    var div = $("<div/>").addClass("file").attr("id", file.link);
+    var a = $("<a/>").attr("href", file.link);
+    a.embedly({
+        key: EMBEDLY_KEY
+    });
+    div.append(a);
+    var date = $("<div/>").text(file.date).addClass("date");
+    div.append(date);
+    var link = $("<div/>").text("Show");
+    div.append(link);
+    return div;
+}
+
 function present_files(files) {
     $("#view").fadeOut(function() {
         $(this).empty();
 
         for (var i = 0; i < files.length; i++) {
-            var div = $("<div/>").addClass("file").attr("id", files[0].link);
-            var iframe = $("<iframe/>").attr("src", files[0].link).addClass("iframe");
-            div.append(iframe);
-            var date = $("<div/>").text(files[i].date).addClass("date");
-            div.append(date);
-            var link = $("<div/>").text("Show");
-            div.append(link);
+            var div;
+            if (isGoogleDriveFile(files[i].link)) {
+                div = prepareGooglePreview(files[i]);
+            } else {
+                div = prepareOtherFilePreview(files[i]);
+            }
             $(this).append(div);
         }
         $(this).fadeIn();
