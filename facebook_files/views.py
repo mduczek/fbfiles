f8de044414601025071909a413cbba70c554c228
Milosz Swizdor <m.swizdor@gmail.com>
2014-10-25 17:49:38 +0200

    Group view

--- facebook_files/views.py
+++ facebook_files/views.py
@@ -6,11 +6,34 @@ from django.views.generic import ListView
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'facebook_files/index.html')
+    # return HttpResponse("Hello, world. You're at the polls index.")
 
-class FileView(ListView):
+class GroupView(ListView): # change to GroupView
 
-    template_name = 'facebook_files/index.html'
+    template_name = 'facebook_files/folder_list.html'
+    context_object_name = 'group'
+
+    def sync_groups(self, group_ids):
+        matched_groups = Group.objects.filter(folder_id__in = group_ids)
+        new_groups = []
+
+        old_group_ids = [group.folder_id for group in matched_groups]
+        for group_id in group_ids:
+            if group_id not in old_group_ids:
+                new_group = Group(name = group_id, folder_id = group_id, parent = None) # todo: change name to sth fancy
+                new_groups.append(new_group)
+                new_group.save()
+
+        return matched_groups + new_groups
 
     def get_queryset(self):
-        return []
+        if 'group_ids' not in self.request.POST:
+            return []
+
+        group_ids = self.request.POST.get('group_ids')
+        return  self.sync_groups(group_ids)
+
+    def get_context_data(self):
+        context = super(FileView, self).get_context_data(**kwargs)
+        context['folders'] = self.get_queryset()
