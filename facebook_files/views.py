0be99a00c1a971a31b2191513bd1725ad3c925b5
michald <mduczynski@student.uw.edu.pl>
2014-10-25 19:43:31 +0200

    njnjkn

--- facebook_files/views.py
+++ facebook_files/views.py
@@ -13,24 +13,27 @@ def index(request):
 def groups(request):
     if 'groups' not in request.POST:
         return json.dumps(request.POST)
-    groups = request.POST.get('groups')
-    sync_groups(groups)
+    groups_str = request.POST.get('groups')
+    groups_json = json.loads(groups_str)
+
     context = {}
-    context['folders'] = groups
+    context['folders'] = sync_groups(groups_json['groups']['data'])
     return render(request, 'facebook_files/folder_list.html', context)
 
 def sync_groups(groups):
-        group_ids = [group['data']['id'] for group in groups]
+    from datetime import datetime
+    group_ids = [group['id'] for group in groups]
 
-        matched_groups = Group.objects.filter(folder_id__in = group_ids)
-        new_groups = []
+    matched_groups = Group.objects.filter(folder_id__in = group_ids)
+    new_groups = []
 
-        old_group_ids = [group.folder_id for group in matched_groups]
-        for group in groups:
-            group_id = group['data']['id']
-            group_name = group['data']['name']
-            if group_id not in old_group_ids:
-                new_group = Group(name = group_name, folder_id = group_id, parent = None)
-                new_groups.append(new_group)
-                new_group.save()
+    old_group_ids = [group.folder_id for group in matched_groups]
+    for group in groups:
+        group_id = group['id']
+        group_name = group['name']
+        if group_id not in old_group_ids:
+            new_group = Group(name = group_name, folder_id = group_id, parent = None, last_modified=datetime.now())
+            new_groups.append(new_group)
+            new_group.save()
 
+    return matched_groups + new_groups
