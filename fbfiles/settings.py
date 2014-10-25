c7e40566c6076effb7169fedbf57f8c995f98c89
michald <mduczynski@student.uw.edu.pl>
2014-10-25 15:16:57 +0200

    fbfiles3

--- fbfiles/settings.py
+++ fbfiles/settings.py
@@ -50,16 +50,6 @@ ROOT_URLCONF = 'fbfiles.urls'
 WSGI_APPLICATION = 'fbfiles.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -79,6 +69,7 @@ USE_TZ = True
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
