f9d971ed752f46c8d05112688c4f4fa62f87090c
gagata <agata.cieplik@gmail.com>
2014-10-26 01:22:40 +0200

    links

--- facebook_files/static/fb_utils.js
+++ facebook_files/static/fb_utils.js
@@ -59,4 +59,7 @@ window.fbAsyncInit = function() {
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
+    if (window.location.pathname === "/") {
+        window.location = "/groups";
+    }
 }
