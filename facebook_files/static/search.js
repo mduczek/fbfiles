7638b6d8c68ebcbca211d9d6255ecad1edfd52d4
Milosz Swizdor <m.swizdor@gmail.com>
2014-10-26 02:40:45 +0200

    Escape dollars etc.

--- facebook_files/static/search.js
+++ facebook_files/static/search.js
@@ -35,9 +35,16 @@ function filter() {
     $('.grid').append(newList.join(''));
 }
 
+RegExp.escape = function
+
+function escape_specials(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
 function filter2() {
     var query = $('#search_box').val();
-    var regex = new RegExp(query, 'i');
+    var escaped_query = escape_specials(query);
+    var regex = new RegExp(escaped_query, 'i');
 
     $('.folder').each(function() {
         if ($(this).find('.name').html().match(regex) || filter == null) {
@@ -46,37 +53,6 @@ function filter2() {
             $(this).hide();
         }
     });
-/*
-    var groups = JSON.parse(localStorage.getItem('groups'));
-    for (var groupId in groups) {
-
-
-        var group = groups[groupId];
-        if (group['name'].match(regex) || filter == null) {
-            var nameTag = $('<div>', {class: 'name'});
-            nameTag.html(group['name']);
-
-            var updateTag = $('<div>', {class: 'date'});
-            updateTag.html(group['date']);
-
-            var idTag = $('<div>', {class: 'folder', id: group['id']});
-            var iconTag = $('<div>', {class: 'icon'});
-
-            var newGroup = idTag
-                .append(iconTag)
-                .append(nameTag)
-                .append(updateTag);
-            
-            // Stars go to the beginning, the rest to the end
-            if (group['starred']) {
-                newList.unshift(newGroup);
-            } else {
-                newList.push(newGroup);
-            }
-        }
-    }
-
-    $('.grid').append(newList.join(''));*/
 }
 
 $(document).ready(function() {
