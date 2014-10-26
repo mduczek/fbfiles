0f5add09bc9363085d23d348f747701f3d440d63
Milosz Swizdor <m.swizdor@gmail.com>
2014-10-26 02:06:10 +0200

    Simple search

--- facebook_files/static/search.js
+++ facebook_files/static/search.js
@@ -35,8 +35,52 @@ function filter() {
     $('.grid').append(newList.join(''));
 }
 
+function filter2() {
+    var query = $('#search_box').val();
+    var regex = new RegExp(query, 'i');
+
+    $('.folder').each(function() {
+        if ($(this).find('.name').html().match(regex) || filter == null) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
+    });
+/*
+    var groups = JSON.parse(localStorage.getItem('groups'));
+    for (var groupId in groups) {
+
+
+        var group = groups[groupId];
+        if (group['name'].match(regex) || filter == null) {
+            var nameTag = $('<div>', {class: 'name'});
+            nameTag.html(group['name']);
+
+            var updateTag = $('<div>', {class: 'date'});
+            updateTag.html(group['date']);
+
+            var idTag = $('<div>', {class: 'folder', id: group['id']});
+            var iconTag = $('<div>', {class: 'icon'});
+
+            var newGroup = idTag
+                .append(iconTag)
+                .append(nameTag)
+                .append(updateTag);
+            
+            // Stars go to the beginning, the rest to the end
+            if (group['starred']) {
+                newList.unshift(newGroup);
+            } else {
+                newList.push(newGroup);
+            }
+        }
+    }
+
+    $('.grid').append(newList.join(''));*/
+}
+
 $(document).ready(function() {
-    $('#search_box').keyup(filter);
+    $('#search_box').keyup(filter2);
 });
 
 
