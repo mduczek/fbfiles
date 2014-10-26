709b6d71a0e7f704ec437c60aab4ff1bf8c69d8c
michald <mduczynski@student.uw.edu.pl>
2014-10-26 03:50:20 +0100

    mkmk

--- facebook_files/static/fb_utils.js
+++ facebook_files/static/fb_utils.js
@@ -59,7 +59,7 @@ window.fbAsyncInit = function() {
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
-    $('.fb_iframe_widget').style("display","none");
+    $('.fb_iframe_widget').css("display","none");
     if (window.location.pathname === "/") {
         window.location = "/groups";
     } else {
